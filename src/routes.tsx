f7fc36052b5f859fc70ed1b2fc9a600b17143875
mayuripy <115979827+mayuripy@users.noreply.github.com>
2024-04-02 15:56:55 +0530

    add routes

---
+++ src/routes.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./page/home";
+import Error from "./page/error";
+
+
+export const router =createBrowserRouter([
+   {
+    path:"/",
+    element:<Home />,
+    errorElement:<Error />
+   },
+
+   {
+    path:"/",
+    element:<Home />,
+    errorElement:<Error />
+   },
+   {
+    path:"/",
+    element:<Home />,
+    errorElement:<Error />
+   },
+   {
+    path:"/",
+    element:<Home />,
+    errorElement:<Error />
+   },
+   {
+    path:"/",
+    element:<Home />,
+    errorElement:<Error />
+   },
+   {
+    path:"/",
+    element:<Home />,
+    errorElement:<Error />
+   },
+   {
+    path:"/",
+    element:<Home />,
+    errorElement:<Error />
+   },
+]);
